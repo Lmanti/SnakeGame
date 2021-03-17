65f0fb95338ed11e9066290dcb316077073b119c
Lmanti <luisangelherreramantilla@gmail.com>
2021-03-17 00:04:08 -0500

    Game finished

--- Js/Game.js
+++ Js/Game.js
@@ -1,10 +1,19 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js"
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from "./snake.js"
+import { update as updateFood, draw as drawFood } from "./food.js"
+import { outsideGrid } from "./grid.js"
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 
 function main(currentTime) {   // se coloca el movimiento de la serpiente en tiempo real.
+    if (gameOver) {
+        if (confirm('Game Over!, You lost!... Press OK to restart!')) {
+            window.location = "./SnakeGame.html"
+        }
+        return 
+    }
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -19,9 +28,16 @@ window.requestAnimationFrame(main);
 
 function update() {
     updateSnake();
+    updateFood();
+    checkDeath();
 }
 
 function draw() {
     gameBoard.innerHTML = "" // para que dibuje sobre el mismo segmento.
     drawSnake(gameBoard);
+    drawFood(gameBoard);
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ A classic snake game builded from zero by me.
 
 1. Create the game environment using HTML and CSS.
 2. Create the snake and the inputs.
+3. Create the food.
+4. Create the conditions.
+5. game finshed.
